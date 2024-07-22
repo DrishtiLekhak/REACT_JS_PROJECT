bb434d0baac7da1413150902c2f92e47715b6e01
DrishtiLekhak <153051778+DrishtiLekhak@users.noreply.github.com>
2024-07-22 14:22:23 +0545

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Jobs from './pages/Jobs'
+import Footer from './components/Footer'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import SingleJobPage from './pages/SingleJobPage'
+import AddJob from './pages/AddJob'
+import {Toaster} from 'react-hot-toast'
+import EditJob from './pages/EditJob'
+const App = () => {
+
+  return (
+
+   <>
+   <Toaster/>
+    <Navbar />
+  
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/jobs' element={<Jobs/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/job/:id' element={<SingleJobPage/>}/>
+      <Route path='/addjob' element={<AddJob/>}/>
+      <Route path='/editjob' element={<EditJob/>}/>
+
+    </Routes>
+
+    <Footer/>
+   </>
+
+  )
+}
+
+export default App
---
+++ src/components/JobCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const JobCard = ({job}) => {
+  // const []
+  return (
+    <div className='border shadow-md rounded-xl p-4'> 
+    
+
+    <div className='space-y-3'>
+        <h1 className='text-gray-600 my-2'>{job.type}</h1>
+        <h3 className='text-xl font-bold'>{job.title}</h3>
+       <p>{job.description}</p>
+         <p className='text-indigo-500 mb-5'>{job.salary}</p>
+    </div>
+
+    <div className=' border border-gray-100 my-5'> </div>
+
+    <div className='flex justify-between items-center'>
+        <p className='text-orange-400'>{job.location}</p>
+
+        <Link to={`/job/${job.id}`}>
+        <button className='text-white bg-teal-500 p-2 rounded-md'>
+            Read More
+        </button>
+        </Link>
+
+    </div>
+
+
+    </div>
+  )
+}
+
+export default JobCard
